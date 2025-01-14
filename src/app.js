01d56029952e7ff4e9afdd4cb3160a58ae70a6b8
Kirill Pochevalov <juget@vk.com>
2025-01-14 22:51:39 +0400

    Настроена добавленная библиотека

--- src/app.js
+++ src/app.js
@@ -1,25 +1,20 @@
-<script src="./node_modules/axios/dist/axios.min.js"></script>
-
-const res = await axios.get("http://ya.ru")
-console.log(res)
-
 // Обработчик события для формы добавления задач
 document.getElementById('task-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Предотвращаем перезагрузку страницы
-    
+
     var taskInput = document.getElementById('task-input');
     var taskText = taskInput.value;
 
     if (taskText) {
         var tasksList = document.getElementById('tasks-list');
-        
+
         // Создаём новый элемент списка
         var listItem = document.createElement('li');
         listItem.textContent = taskText;
 
         // Добавляем новый элемент в список
         tasksList.appendChild(listItem);
-        
+
         // Очищаем поле ввода
         taskInput.value = '';
     }
